c6257db9f6c8f41eef4a1b02dce50d1e1dccf83c
Aldrin Capuchino <101033787+pnkskff@users.noreply.github.com>
2022-05-26 15:42:32 -0400

    Add files via upload

---
+++ Solution 2 Opposite Number.js
@@ -0,0 +1,17 @@
+// CodeWars Solutions
+
+// 2. Opposite Number - Very simple, given an integer or a floating-point number, find its opposite.
+
+// Parameters: Positive, Negative, Integers
+// Return: Return Opposite Integer
+// Example: Opposite of 1 is -1
+// Pseudocode:
+// 1. Create Function
+// 2. Return Opposite of Integer that's specified within function
+// 3. Call function
+
+function opposite(number) {
+  return (-number);
+}
+
+opposite (1)
