7953013793dc5f4ef7ea27ecc4263ffe39c7d586
Aldrin Capuchino <101033787+pnkskff@users.noreply.github.com>
2022-05-27 19:38:09 -0400

    Add files via upload

---
+++ Solution 3 Num to Str.js
@@ -0,0 +1,17 @@
+// CodeWars Solutions
+
+// 3.  We need a function that can transform a number into a string. What ways of achieving this do you know?
+
+// Parameters: Positive, Integers, String
+// Return: Return a String from a Number
+// Example: 4 turns to '4'
+// Pseudocode:
+// 1. Create Function
+// 2. Return a Num to a Str
+// 3. Call Function
+
+function numberToString(num) {
+  return num.toString()
+}
+
+numberToString (4)
