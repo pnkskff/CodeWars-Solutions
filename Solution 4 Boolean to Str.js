f66c773f693cf405ab31c0da52947295286cb053
Aldrin Capuchino <101033787+pnkskff@users.noreply.github.com>
2022-05-28 22:43:48 -0400

    Add files via upload

---
+++ Solution 4 Boolean to Str.js
@@ -0,0 +1,18 @@
+// CodeWars Solutions
+
+// 1. Convert Boolean to Strings:
+// Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
+// Parameters: Boolean Value, String
+// Return: Return 'Yes' Str for True and 'No' Str for False
+// Example: Yes turns to 'Yes', No turns to 'No'
+// Pseudocode:
+// 1. Create Function
+// 2. Return Bool (True or False) as String (Yes or No)
+// 3. Call Function
+
+function boolToWord( bool ){
+  return bool ? 'Yes' : 'No'
+}
+
+boolToWord (true)
