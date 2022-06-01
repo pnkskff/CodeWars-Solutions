780110844d42dd8257ef44c9866c906bf086987b
Aldrin Capuchino <101033787+pnkskff@users.noreply.github.com>
2022-05-31 21:14:06 -0400

    Add files via upload

---
+++ Solution 5 Reversed String.js
@@ -0,0 +1,20 @@
+// CodeWars Solutions
+
+// 1. Complete the solution so that it reverses the string passed into it.
+
+// Parameters: Strings
+// Return: Return as Reversed
+// Example: 'world' => 'dlrow'
+// Pseudocode:
+// 1. Write Function
+// 2. Insert String as Var
+// 3. Split String
+// 4. Reverse String
+// 5. Join String
+// 6. Call Function
+
+function solution(str){
+  return str.split ('').reverse().join ('')
+}
+
+solution('world')
