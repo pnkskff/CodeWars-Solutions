6d634ec671d4abff6007bc14c6bb0a2623e484b0
Aldrin Capuchino <101033787+pnkskff@users.noreply.github.com>
2022-06-07 22:11:05 -0400

    Delete CodeWars Solutions (2022).js

--- CodeWars Solutions (2022).js
+++
@@ -1,7 +0,0 @@
-// CodeWars Solutions
-// URL: https://www.codewars.com/kata/search/javascript?q=&r[]=-8&tags=Fundamentals&beta=false&order_by=total_completed%20desc
-
-// 1. Use P.R.E.P. when writing Solutions
-// 2. Spend 20 mins then look at Answers, analyze Solutions
-// 3. Create JS file for tracking
-// 4. Push to Github for an Online Repo
