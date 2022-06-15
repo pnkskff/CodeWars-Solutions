3c346b873fd5b0ee53e07383475072cd81678671
Aldrin Capuchino <101033787+pnkskff@users.noreply.github.com>
2022-06-14 22:48:41 -0400

    Update Solution 9 Remove First and Last Character.js

--- Solution 9 Remove First and Last Character.js
+++ Solution 9 Remove First and Last Character.js
@@ -7,7 +7,8 @@
 // Example: If String = 'Hello', Function will Shift and Pop 'H' and 'o', resulting in new String of 'ell'
 // Pseudocode:
 // 1. Create Function
-// 2. Use Slice method to Shift 1st Character and Pop Last Character
+// 2. Use Slice method to Shift 1st Character and Pop Last Character, see below
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/slice
 // 3. Call Function
 
 function removeChar(str){
