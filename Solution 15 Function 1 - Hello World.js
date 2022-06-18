339fc38828739ef9079cddcb09618005c1f382d2
Aldrin Capuchino <101033787+pnkskff@users.noreply.github.com>
2022-06-18 16:11:46 -0400

    Add files via upload

---
+++ Solution 15 Function 1 - Hello World.js
@@ -0,0 +1,21 @@
+// CodeWars Solutions
+
+// 15. Function 1 - Hello World:
+// Make a simple function called greet that returns the most-famous "hello world!".
+
+// Parameters: String
+// Return: Function to return 'hello world!'
+// Example: greet('hello world!')
+// Pseudocode:
+// 1. Create const
+// 2. Create Function
+// 3. Use return to return words variable
+// 4. Call Function
+
+const words = ('hello world!')
+
+function greet () {
+  return words
+}
+
+greet (words)
