01b9091cd5edaa51ae7c0355ad6262a1f0124aa4
Aldrin Capuchino <101033787+pnkskff@users.noreply.github.com>
2022-06-19 15:32:35 -0400

    Add files via upload

---
+++ Solution 16 Convert String to Number.js
@@ -0,0 +1,19 @@
+// CodeWars Solutions
+
+// 16. Convert String into Number
+// We need a function that can transform a string into a number. What ways of achieving this do you know?
+
+// Parameters: String
+// Return: Turn Str to Num
+// Example: '1234' turns to 1234
+// Pseudocode:
+// 1. Create Function
+// 2. User Number() to convert str, see below
+// 3. https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number
+// 4. Call Function
+
+const stringToNumber = function(str){
+  return Number(str)
+}
+
+stringToNumber('1234')
