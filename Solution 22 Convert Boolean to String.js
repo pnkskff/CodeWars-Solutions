5717a6503f6ff7d8efad8309fbb28db5675f1c03
Aldrin Capuchino <101033787+pnkskff@users.noreply.github.com>
2022-07-03 18:05:29 -0400

    Add files via upload

---
+++ Solution 22 Convert Boolean to String.js
@@ -0,0 +1,19 @@
+// CodeWars Solutions
+
+// 22. Convert Boolean to String
+// Implement a function which convert the given boolean value into its string representation.
+
+// Parameters: Boolean, String
+// Return: Return a String from a Boolean
+// Example: true turns to 'true'
+// Pseudocode:
+// 1. Create Function
+// 2. Use toString method to convert input to String, see below
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/toString
+// 3. Call Function
+
+function booleanToString(b){
+  return b.toString()
+}
+
+booleanToString(true)
