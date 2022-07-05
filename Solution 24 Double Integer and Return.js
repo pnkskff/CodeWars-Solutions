286dfa16fd7623b53d9ec8873e7e55c7602c6024
Aldrin Capuchino <101033787+pnkskff@users.noreply.github.com>
2022-07-05 19:58:04 -0400

    Add files via upload

---
+++ Solution 24 Double Integer and Return.js
@@ -0,0 +1,19 @@
+// CodeWars Solutions
+
+// 24. Double an Integer and Return
+// You need to double the integer and return it..
+
+// Parameters: Integer
+// Return: Double given Integer
+// Example: 2 turns to 4
+// Pseudocode:
+// 1. Create Function
+// 2. Use * Operator to double Integer, see below
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Multiplication
+// 3. Call Function
+
+function doubleInteger(i) {
+  return i * 2
+}
+
+doubleInteger(2)
