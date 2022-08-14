0f6256aa1678cb05ae53d3ece4cd527b15424899
Aldrin Capuchino <101033787+pnkskff@users.noreply.github.com>
2022-08-14 18:55:27 -0400

    Add files via upload

---
+++ Solution 8 - 7 Kyu - List Filtering.js
@@ -0,0 +1,31 @@
+// CodeWars Solutions
+
+// 8. 7 Kyu - List Filtering
+// In this kata you will create a function that takes a list of non-negative integers and strings and returns a new list with the strings filtered out.
+
+// Parameters: Array, Positive, Integers, Strings
+// Return: New Array with String filtered out
+// Example: [1, 2, 'a', 'b'] -> [1, 2]
+// Pseudocode:
+// 1. Create Function
+// 2. Use Filter method to narrow down Parameters to only the Number, see below
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/filter
+// 3. Use typeOf operator to specify that the Filter should only return Number, see below
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/typeof
+// 4. Use Strict Equality operator to specify that Parameters should only be equal in type and value to Number, and not String, see below
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Strict_equality
+// 5. Call Function
+
+// NOTE: Submitting this Solution to CodeWars results in a Fail, not sure why
+
+function filterList(list) {
+  return list.filter( num => typeof num === 'number' )
+}
+
+filterList([27, 89, 69, 21, 'Kanye West', 100, 'Jay-Z'])
+
+// CodeWars Solution that works:
+
+// function filter_list(l) {
+//  return l.filter(v => typeof v == "number")
+// }
