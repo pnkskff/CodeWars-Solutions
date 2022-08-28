6b47e75edb0c4f3dd9c526ea5dc6033b6703a2f4
Aldrin Capuchino <101033787+pnkskff@users.noreply.github.com>
2022-08-27 20:44:38 -0400

    Update and rename Solution 11 - 7 Kyu - Shortest Word.js to Solution 12 - 7 Kyu - Shortest Word.js

--- Solution 12 - 7 Kyu - Shortest Word.js
+++ Solution 12 - 7 Kyu - Shortest Word.js
@@ -1,6 +1,6 @@
 // CodeWars Solutions
 
-// 11. 7 Kyu - Shortest Word
+// 12. 7 Kyu - Shortest Word
 // Simple, given a string of words, return the length of the shortest word(s).
 // String will never be empty and you do not need to account for different data types.
 
