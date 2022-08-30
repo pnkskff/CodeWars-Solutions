d1c6e2a2c5d47b3fa129d2fab35fc292c6dfeb50
Aldrin Capuchino <101033787+pnkskff@users.noreply.github.com>
2022-08-30 15:20:14 -0400

    Add files via upload

---
+++ Solutions Template.js
@@ -0,0 +1,17 @@
+// CodeWars Solutions
+
+// . 7 Kyu - 
+// 
+
+// Parameters: 
+// Return: 
+// Example: 
+// Pseudocode:
+// 1. Create Function
+// 2. 
+// 3. 
+// 4. 
+// 5. 
+// 6. 
+// 7. 
+// 8. Call Function
