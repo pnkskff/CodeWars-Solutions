fff12bd719f1cce9564fa3d73d0249118d9f0c3a
Aldrin Capuchino <101033787+pnkskff@users.noreply.github.com>
2022-08-31 16:24:23 -0400

    Update Solutions Template.js

--- Solutions Template.js
+++ Solutions Template.js
@@ -2,30 +2,30 @@
 
 // I. FORMAT: 
 
-//    1. CodeWars Solutions
+//    CodeWars Solutions
 
-//    2. Number. Solution Number - Kata Kyu - Kata Name 
-//    3. Kata Story
+//    Number. Solution Number - Kata Kyu - Kata Name 
+//    Kata Story
 
-//    a. Parameters: 
-//    b. Return: 
-//    c. Example: 
-//    d. Pseudocode:
-//       1. Create Function
-//       Insert documentation URL if applicable.
-//       2. 
-//       3. 
-//       4. 
-//       5. 
-//       6. 
-//       7. 
-//       8. Call Function
+//    Parameters: 
+//    Return: 
+//    Example: 
+//    Pseudocode:
+//    1. Create Function
+//    Insert documentation URL if applicable.
+//    2. 
+//    3. 
+//    4. 
+//    5. 
+//    6. 
+//    7. 
+//    8. Call Function
 
 //    Insert working Solution below, regardless if it passes CodeWars Tests or not:
-//    4. INSERT CODE
+//    INSERT CODE
 
 //    If applicable, insert Solution that works with CodeWars below:
-//    5. INSERT CODE
+//    INSERT CODE
 
 // II. NOTE:
 //    1. Format of Number and Solution Number is based on Numbering System within this Repo.
