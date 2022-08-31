54c85ac5b482e209545517fce023f4cb830af2d5
Aldrin Capuchino <101033787+pnkskff@users.noreply.github.com>
2022-08-31 16:23:00 -0400

    Update CodeWars Solutions.js

--- CodeWars Solutions.js
+++ CodeWars Solutions.js
@@ -1,20 +1,20 @@
 // CodeWars Solutions
 
-// 1. URL: 
-//    8 Kyu - https://www.codewars.com/kata/search/javascript?q=&r[]=-8&tags=Fundamentals&beta=false&order_by=total_completed%20desc
-//    7 Kyu - https://www.codewars.com/kata/search/javascript?q=&r%5B%5D=-7&tags=Fundamentals&beta=false&order_by=total_completed%20desc
+// I. URL: 
+//    1. 8 Kyu - https://www.codewars.com/kata/search/javascript?q=&r[]=-8&tags=Fundamentals&beta=false&order_by=total_completed%20desc
+//    2. 7 Kyu - https://www.codewars.com/kata/search/javascript?q=&r%5B%5D=-7&tags=Fundamentals&beta=false&order_by=total_completed%20desc
 
-// 2. INSTRUCTIONS:
-//    a. Use P.R.E.P. when writing Solutions.
-//    b. Insert MDN doc for Functions, etc.
-//    c. Spend 20 mins then look at Answers, analyze Solutions.
-//    d. Create JS file for tracking.
-//    e. Push to Github for an Online Repo.
+// II. INSTRUCTIONS:
+//    1. Use P.R.E.P. when writing Solutions.
+//    2. Insert MDN doc for Functions, etc.
+//    3. Spend 20 mins then look at Answers, analyze Solutions.
+//    4. Create JS file for tracking.
+//    5. Push to Github for an Online Repo.
 
-// 3. FURTHER INFO:
-//    a. This Doc contains instructions on completing Kata Challenges.
-//    b. CodeWars Solutions last updated on: AUGUST 2022
-//    c. Author: pnkskff
+// III. FURTHER INFO:
+//    1. This Doc contains instructions on completing Kata Challenges.
+//    2. CodeWars Solutions last updated on: AUGUST 2022
+//    3. Author: pnkskff
 //               https://github.com/pnkskff
 //               https://drincapdev.netlify.app/
 //               https://drincapapps.netlify.app/
