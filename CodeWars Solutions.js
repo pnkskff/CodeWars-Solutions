b9030e8308a6e90e7ca8a0bc0801a7d5017fa9f0
Aldrin Capuchino <101033787+pnkskff@users.noreply.github.com>
2022-08-31 16:26:51 -0400

    Update CodeWars Solutions.js

--- CodeWars Solutions.js
+++ CodeWars Solutions.js
@@ -5,11 +5,11 @@
 //    2. 7 Kyu - https://www.codewars.com/kata/search/javascript?q=&r%5B%5D=-7&tags=Fundamentals&beta=false&order_by=total_completed%20desc
 
 // II. INSTRUCTIONS:
-//    1. Use P.R.E.P. when writing Solutions.
-//    2. Insert MDN doc for Functions, etc.
-//    3. Spend 20 mins then look at Answers, analyze Solutions.
-//    4. Create JS file for tracking.
-//    5. Push to Github for an Online Repo.
+//     1. Use P.R.E.P. when writing Solutions.
+//     2. Insert MDN doc for Functions, etc.
+//     3.  Spend 20 mins then look at Answers, analyze Solutions.
+//     4. Create JS file for tracking.
+//     5. Push to Github for an Online Repo.
 
 // III. FURTHER INFO:
 //      1. This Doc contains instructions on completing Kata Challenges.
