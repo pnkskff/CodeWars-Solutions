64916516a0d79e05d0970d3100e3f7e3c9cd3c3f
Aldrin Capuchino <101033787+pnkskff@users.noreply.github.com>
2022-08-31 16:42:42 -0400

    Update CodeWars Solutions.js

--- CodeWars Solutions.js
+++ CodeWars Solutions.js
@@ -1,15 +1,24 @@
 // CodeWars Solutions
 
 // I. URL: 
-//    1. 8 Kyu JS - https://www.codewars.com/kata/search/javascript?q=&r[]=-8&tags=Fundamentals&beta=false&order_by=total_completed%20desc
-//    2. 7 Kyu JS - https://www.codewars.com/kata/search/javascript?q=&r%5B%5D=-7&tags=Fundamentals&beta=false&order_by=total_completed%20desc
+//    1. 8 Kyu JS
+//       https://www.codewars.com/kata/search/javascript?q=&r[]=-8&tags=Fundamentals&beta=false&order_by=total_completed%20desc
+//    2. 7 Kyu JS
+//       https://www.codewars.com/kata/search/javascript?q=&r%5B%5D=-7&tags=Fundamentals&beta=false&order_by=total_completed%20desc
 
 // II. INSTRUCTIONS:
-//     1. Use P.R.E.P. when writing Solutions.
+//     1. Use P.R.E.P. method when troubleshooting and writing Solutions.
+//        a. See ReadMe for further information:
+//           https://github.com/pnkskff/CodeWars-Solutions/blob/main/README.md
+
 //     2. Insert MDN/3rd Party source url for documentation on: Definitions, Properties, Methods, etc.
-//     3. Spend 20 mins then look at Answers, analyze Solutions.
-//     4. Create JS file for tracking.
-//     5. Push to GitHub for an Online Repo.
+
+//     3. Spend 20 mins on completing Kata, look at Answers if cannot complete, then analyze Solutions to internalize knowledge.
+
+//     4. Create JS file for tracking via Text Editor of choice.
+
+//     5. Push to GitHub into an Online Repo:
+//        a. https://github.com/pnkskff/CodeWars-Solutions
 
 // III. FURTHER INFO:
 //      1. This Doc contains instructions on completing Kata Challenges.
