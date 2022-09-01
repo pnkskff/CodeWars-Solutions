06981633c5239b3d9f2a539b77c27c53f595f714
Aldrin Capuchino <101033787+pnkskff@users.noreply.github.com>
2022-09-01 13:21:54 -0400

    Update Solutions Template.js

--- Solutions Template.js
+++ Solutions Template.js
@@ -1,6 +1,7 @@
 // Solutions Template
 
 // I. FORMAT: 
+
 //    CodeWars Solutions
 
 //    Number. Solution Number - Kata Kyu - Kata Name 
@@ -27,15 +28,17 @@
 //    INSERT CODE
 
 // II. NOTE:
+
 //     1. Format of Number and Solution Number is based on Numbering System within CodeWars Solutions Repo.
 
 // III. FURTHER INFO:
+
 //      1. CodeWars Solutions Repo URL: 
 //         https://github.com/pnkskff/CodeWars-Solutions
 
 //      2. This Doc is a Template for CodeWars Solutions on this Repo.
 
-//      3. Solutions Template last updated on: AUGUST 2022
+//      3. Solutions Template last updated on: SEPTEMBER 2022
 
 //      4. Author: pnkskff
 //                 https://github.com/pnkskff
