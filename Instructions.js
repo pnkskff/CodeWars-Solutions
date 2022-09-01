db376b2c9de9e57a5f16a84b391b1616c60a463b
Aldrin Capuchino <101033787+pnkskff@users.noreply.github.com>
2022-09-01 13:22:02 -0400

    Update Instructions.js

--- Instructions.js
+++ Instructions.js
@@ -1,12 +1,14 @@
 // Instructions
 
 // I. URL: 
+
 //    1. 8 Kyu JS
 //       https://www.codewars.com/kata/search/javascript?q=&r[]=-8&tags=Fundamentals&beta=false&order_by=total_completed%20desc
 //    2. 7 Kyu JS
 //       https://www.codewars.com/kata/search/javascript?q=&r%5B%5D=-7&tags=Fundamentals&beta=false&order_by=total_completed%20desc
 
 // II. INSTRUCTIONS:
+
 //     1. Within Kata in CodeWars, use P.R.E.P. method when troubleshooting and writing Solutions.
 //        a. See ReadMe for further information:
 //           https://github.com/pnkskff/CodeWars-Solutions/blob/main/README.md
@@ -20,12 +22,13 @@
 //     4. Push to GitHub into an Online Repo.
 
 // III. FURTHER INFO:
+
 //      1. CodeWars Solutions Repo URL: 
 //         https://github.com/pnkskff/CodeWars-Solutions
 
 //      2. This Doc contains instructions on completing Kata Challenges.
 
-//      3. Instructions last updated on: AUGUST 2022
+//      3. Instructions last updated on: SEPTEMBER 2022
 
 //      4. Author: pnkskff
 //                 https://github.com/pnkskff
