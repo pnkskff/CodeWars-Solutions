c320a1b2c28cf2d36dbc4331c5d1c50ce2c45511
Aldrin Capuchino <101033787+pnkskff@users.noreply.github.com>
2022-09-20 14:16:22 -0400

    Update Solution 9 - 7 Kyu - Jaden Casing Strings.js

--- 7 Kyu Solutions/Solution 9 - 7 Kyu - Jaden Casing Strings.js
+++ 7 Kyu Solutions/Solution 9 - 7 Kyu - Jaden Casing Strings.js
@@ -33,6 +33,6 @@ toJadenCase('I am the Dope Man ya boi wear corduroy')
 
 // CodeWars Solution that works:
 
-// String.prototype.toJadenCase = function () {
-//   return this.split(' ').map(w => w[0].toUpperCase() + w.slice(1)).join(' ')
-// }
+String.prototype.toJadenCase = function () {
+  return this.split(' ').map(w => w[0].toUpperCase() + w.slice(1)).join(' ')
+}
