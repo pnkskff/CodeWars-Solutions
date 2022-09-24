5a502a943c94e8b1abdcb9e8ff629c0e71498f63
Aldrin Capuchino <101033787+pnkskff@users.noreply.github.com>
2022-09-24 17:31:43 -0400

    Update Staging Doc.js

--- Staging Doc.js
+++ Staging Doc.js
@@ -152,14 +152,18 @@ isTriangle(2,12,27)
 //             longest(a, b) -> 'abcdefklmopqwxy'
 //    Pseudocode:
 //    1. Create Function
-//    Insert MDN/3rd Party source url within JS file for documentation on: Definitions, Properties, Methods, etc.
-//    2. 
-//    3. 
-//    4. 
-//    5. 
-//    6. 
-//    7. 
-//    8. Call Function
+//    2. Use Array.from() method to create a new Array from an iterable Object, see below
+//    https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/from
+//    3. Within Array.from(), use new Operator and Set Object to create a new collection of values from pre-existing Objects, see below
+//    https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/new
+//    https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Set
+//    4. Within Set Object, use Addition Operator to concatenate results from both Objects into 1 Object, see below 
+//    https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Addition
+//    5. Use Sort Method to then arrange the result of Set into Ascending Order, see below
+//    https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
+//    6. Use Join Method to join the Letters back together into 1 String, see below
+//    https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/join
+//    7. Call Function
 
 function longestStr(a, b) {
     return Array.from( new Set (a + b) ).sort().join('')
