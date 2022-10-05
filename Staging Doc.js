336b663b29c2ed091ecbef84cfa4e58e579383c4
Aldrin Capuchino <101033787+pnkskff@users.noreply.github.com>
2022-10-04 22:22:03 -0400

    Update Staging Doc.js

--- Staging Doc.js
+++ Staging Doc.js
@@ -112,6 +112,36 @@ likes(['Kanye', 'Kendrick', 'Drake', 'Kid Cudi'])
 
 // ------------------------------
 
+//    CodeWars Solutions
+
+//    Number. Solution Number - Kata Kyu - Kata Name 
+//    Kata Story
+
+//    Parameters: 
+//    Return: 
+//    Example: 
+//    Pseudocode:
+//    1. Create Function
+//    Insert MDN/3rd Party source url within JS file for documentation on: Definitions, Properties, Methods, etc.
+//    2. 
+//    3. 
+//    4. 
+//    5. 
+//    6. 
+//    7. 
+//    8. Call Function
+
+//    Insert working Solution below, regardless if it passes CodeWars Tests or not:
+//    INSERT CODE
+
+//    If applicable, insert Troubleshooting Steps below, if User is able to fix Code to get a Pass in CodeWars:
+//    INSERT DETAILS OF SOLUTIONS
+
+//    If applicable, insert Alternate Solution or Solution that works with CodeWars below:
+//    INSERT CODE
+
+// ------------------------------
+
 // II. FURTHER INFO:
 
 //     1. CodeWars Solutions Repo URL: 
