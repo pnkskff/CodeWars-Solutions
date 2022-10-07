f4b56406aedd69faf8bda12ca3a2a144e8ffad61
Aldrin Capuchino <101033787+pnkskff@users.noreply.github.com>
2022-10-07 12:49:13 -0400

    Update Staging Doc.js

--- Staging Doc.js
+++ Staging Doc.js
@@ -141,13 +141,10 @@ likes(['Kanye', 'Kendrick', 'Drake', 'Kid Cudi'])
 //    8. Call Function
 
 //    Insert working Solution below, regardless if it passes CodeWars Tests or not:
-//    INSERT CODE
 
 //    If applicable, insert Troubleshooting Steps below, if User is able to fix Code to get a Pass in CodeWars:
-//    INSERT DETAILS OF SOLUTIONS
 
 //    If applicable, insert Alternate Solution or Solution that works with CodeWars below:
-//    INSERT CODE
 
 // ------------------------------
 
