2ff1ca5669ad9d4b30e2832133c14fd1d3ad9ac8
Aldrin Capuchino <101033787+pnkskff@users.noreply.github.com>
2022-12-10 11:10:19 -0500

    Update Staging Doc.js

--- Staging Doc.js
+++ Staging Doc.js
@@ -186,6 +186,7 @@ findOdd([0,0,1,2,2,3,3])
 //    2. Use Filter method to iterate through Array, see below
 //    https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/filter
 //    3. Use Arrow function to specify conditions for returning a new Array, see below
+//    https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/Arrow_functions
 //    4. 
 //    5. 
 //    6. 
