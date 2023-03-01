1fcede80f5417d0de5d64859ddc5104841171f06
Aldrin Capuchino <101033787+pnkskff@users.noreply.github.com>
2023-03-01 15:29:27 -0500

    Update Staging Doc.js

--- Staging Doc.js
+++ Staging Doc.js
@@ -234,8 +234,8 @@ function array_diff(a, b) {
 //    CodeWars Solutions
 
 //    4. Solution 4 - 6 Kyu - Counting Duplicates
-//    
-//
+//    Write a function that will return the count of distinct case-insensitive alphabetic characters and numeric digits that occur more than once in the input string. 
+//    The input string can be assumed to contain only alphabets (both uppercase and lowercase) and numeric digits.
 
 //    Parameters: Numbers Only, Array
 //    Return: 
