1555608fa3a1b8819dbb961ffa6870b882c20eda
Aldrin Capuchino <101033787+pnkskff@users.noreply.github.com>
2023-04-20 17:49:43 -0400

    Update Staging Doc.js

--- Staging Doc.js
+++ Staging Doc.js
@@ -224,7 +224,7 @@ function array_diff(a, b) {
 //       a. 6 Kyu - Counting Duplicates
 
 //    2. STATUS:
-//       a. Not submitted to CodeWars.
+//       a. Submitted to CodeWars.
 //       b. Complete Documentation.
 //       c. Not pushed to GitHub.
 //       d. JS File not created on Desktop.
