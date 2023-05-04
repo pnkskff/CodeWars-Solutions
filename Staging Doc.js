f966a065d70625cd183ba5095598a23919a87c57
Aldrin Capuchino <101033787+pnkskff@users.noreply.github.com>
2023-05-04 15:39:46 -0400

    Update Staging Doc.js

--- Staging Doc.js
+++ Staging Doc.js
@@ -307,7 +307,9 @@ duplicateCount('Toyota')
 //    CodeWars Solutions
 
 //    Number. Solution Number - Kata Kyu - Kata Name 
-//    Kata Story
+//    The goal of this exercise is to convert a string to a new string where each character in the new string is "(" 
+//    If that character appears only once in the original string, or ")" if that character appears more than once in the original string. 
+//    Ignore capitalization when determining if a character is a duplicate.
 
 //    Parameters: 
 //    Return: 
