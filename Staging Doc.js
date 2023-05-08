298e744e064bc2f6cb96233ab309823b7304cca0
Aldrin Capuchino <101033787+pnkskff@users.noreply.github.com>
2023-05-08 18:49:50 -0400

    Update Staging Doc.js

--- Staging Doc.js
+++ Staging Doc.js
@@ -307,17 +307,19 @@ duplicateCount('Toyota')
 //    CodeWars Solutions
 
 //    5. Solution 5 - 6 Kyu - Duplicate Encoder 
-//    The goal of this exercise is to convert a string to a new string where each character in the new string is "(" 
+//    The goal of this exercise is to convert a string to a new string where each character in the new string is "(".
 //    If that character appears only once in the original string, or ")" if that character appears more than once in the original string. 
 //    Ignore capitalization when determining if a character is a duplicate.
 
 //    Parameters: 
 //    Return: 
 //    Example: 
-//    "din"      =>  "((("
-//    "recede"   =>  "()()()"
-//    "Success"  =>  ")())())"
-//    "(( @"     =>  "))((" 
+//    "din"      ->  "((("
+//    All characters unique, resulting in only ( to display 3 times.
+//    "recede"   ->  "()()()"
+//    1 character repeats 3 times, results in 3 instances of ).
+//    "Success"  ->  ")())())"
+//    "(( @"     ->  "))((" 
 //    Pseudocode:
 //    1. Create Function
 //    
