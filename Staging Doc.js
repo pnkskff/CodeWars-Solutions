286ccc0fc3729077b80d407a785d38caa0a5392c
Aldrin Capuchino <101033787+pnkskff@users.noreply.github.com>
2023-05-16 15:24:48 -0400

    Update Staging Doc.js

--- Staging Doc.js
+++ Staging Doc.js
@@ -331,7 +331,7 @@ duplicateCount('Toyota')
 //    If that character appears only once in the original string, or ")" if that character appears more than once in the original string. 
 //    Ignore capitalization when determining if a character is a duplicate.
 
-//    Parameters: Alphanumeric String, Special Characters String
+//    Parameters: Alphabet String, Special Characters String
 //    Return: ( if character does not repeat, ) if character/s repeat
 //    Example: 
 //    "din" -> "((("
