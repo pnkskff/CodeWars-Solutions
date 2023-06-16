d9999c290710b707d9d44dc5193203288ed0c74e
Aldrin Capuchino <101033787+pnkskff@users.noreply.github.com>
2023-06-16 18:13:58 -0400

    Update Staging Doc.js

--- Staging Doc.js
+++ Staging Doc.js
@@ -71,9 +71,11 @@ function reverse1(str){
 }
 
 console.log(reverse1("javascript"))
-// Log will show tpircsavaj
+// Log will show 'tpircsavaj'
 
 console.log(reverse1("hello"))
+// Log will show 'olleh'
+
 console.log(reverse1("world"))
 
 // ------------------------------
