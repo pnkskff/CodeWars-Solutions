c9485f1b640e4fff5836d4533d236672edc5a222
Aldrin Capuchino <101033787+pnkskff@users.noreply.github.com>
2023-07-21 18:03:32 -0400

    Update Staging Doc.js

--- Staging Doc.js
+++ Staging Doc.js
@@ -54,6 +54,18 @@
 
 //    
 
+function duplicateEncode(word){
+  return word
+    .toLowerCase()
+    .split('')
+    .map( function (a, i, w) {
+      return w.indexOf(a) == w.lastIndexOf(a) ? '(' : ')'
+    })
+    .join('');
+}
+
+
+
 // ------------------------------
 
 // I. INTERVIEW CODING QUESTIONS:
