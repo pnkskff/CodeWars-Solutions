b8da5cba85713d8e7233993da87a520f08e37ff5
Aldrin Capuchino <101033787+pnkskff@users.noreply.github.com>
2023-08-18 17:08:37 -0400

    Update Staging Doc.js

--- Staging Doc.js
+++ Staging Doc.js
@@ -49,6 +49,7 @@
 //    6. Within Map, write a function that will iterate through the Array and return the first and next instance/s of a recurring character, see below:
 //    https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/indexOf
 //    https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/lastIndexOf
+//    a. 
 //    7. Use Join method to put the characters back together into a String, see below:
 //    https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/join
 //    8. Call Function
